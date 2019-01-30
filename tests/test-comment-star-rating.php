732c032f4c7bb4cdac2c3b472ea64a938a0f5e35
matometaru <matometaru@gmail.com>
2019-01-30 20:43:11 +0900

    投稿タイプのフラグが正常かのテストを追加

---
+++ tests/test-comment-star-rating.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Class CommentStarRatingTest
+ *
+ * @package Commen-star-rating
+ */
+
+/**
+ * CommentStarRatingTest test case.
+ */
+class CommentStarRatingTest extends WP_UnitTestCase {
+
+	private $comment_star_rating;
+	private $cat1;
+	private $post1;
+
+	const ENABLE_POST  = [ 'post' => '1' ];
+	const DISABLE_POST = [ 'post' => '0' ];
+	const ENABLE_EMAIL = [ 'email' => '1' ];
+	const ENABLE_URL   = [ 'url' => '1' ];
+
+	/**
+	 * construct.
+	 */
+	public function __construct( $name = null, array $data = array(), $data_name = '' ) {
+		parent::__construct( $name, $data, $data_name );
+
+		$url = plugins_url( '', __FILE__ );
+		$this->comment_star_rating = new CommentStarRating( $url );
+		$this->setOptions( self::ENABLE_POST );
+	}
+
+	/**
+	 * Set up.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// 投稿を作成.
+		$this->post1 = $this->factory->post->create();
+	}
+
+	/**
+	 * Tear down.
+	 */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	/**
+	 * 設定: オプションを設定する.
+	 *
+	 * @param array $options オプション配列.
+	 */
+	public function setOptions( $options ) {
+		$this->comment_star_rating->update_options( $options );
+	}
+
+	/**
+	 * 管理画面側の設定
+	 */
+	public function test_初回の投稿タイプpostが無効になっているか() {
+		$actual = $this->comment_star_rating->is_enabled_post_type( 'post' );
+		$this->assertEquals( false, $actual );
+	}
+
+	/**
+	 * 現在のページで投稿タイプの有効フラグが立っているか。
+	 */
+	public function test_現在のページで投稿タイプの有効フラグが立っているか() {
+		$this->go_to( '/?p=' . $this->post1 );
+		$post_type = get_post_type();
+		$actual = $this->comment_star_rating->is_enabled_post_type( $post_type );
+		$this->assertEquals( true, $actual );
+	}
+}
