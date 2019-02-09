4d725ba8706a680be62a531ab1290dafb6f384d2
matometaru <matometaru@gmail.com>
2019-02-09 19:49:24 +0900

    文字列で返す_generatorメソッド追加

--- classes/abstract/class.controller.php
+++ classes/abstract/class.controller.php
@@ -17,6 +17,22 @@ class CSR_Controller {
 			return;
 		}
 
-		include( $template_path );
+		include $template_path;
+	}
+
+	protected function _generator( $template, array $args = array() ) {
+		extract( $args );
+		$template_dir  = plugin_dir_path( __FILE__ ) . '../../templates/';
+		$template_path = $template_dir . $template . '.php';
+
+		if ( ! file_exists( $template_path ) ) {
+			return;
+		}
+
+		ob_start();
+		include $template_path;
+		$html = ob_get_contents();
+		ob_end_clean();
+		return $html;
 	}
 }
